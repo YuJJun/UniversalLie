98cc6c81dab7e5d240e3933d3f6c67259308748d
HyunJun Yoo <johnyoo0918@gmail.com>
2018-07-19 19:28:24 -0500

    Added character class with health and strength mods

---
+++ UniversalLIe/Character.h
@@ -0,0 +1,17 @@
+#pragma once
+class Character
+{
+public:
+	Character();
+
+private:
+
+	int health = 100;
+	int currentHealth;
+
+	int strength = 30;
+	int strMod = 0; //Strength modifier
+
+
+};
+
